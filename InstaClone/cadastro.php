7cdc183067fae66b53524899b22bd1abd4a9b643
João Scaleão <j.scaleao@hotmail.com>
2019-06-28 23:49:03 -0300

    Verificação do cadastro e utilização de sessão
    Esse PUSH, faço algumas alterações necessarias na estrutura do codigo no geral para adicionar a verificação de erro do cadastro e sessão. Sujeito a alterações.

--- InstaClone/cadastro.php
+++ InstaClone/cadastro.php
@@ -1,4 +1,7 @@
 <?php
+require_once "./app/conexao.php";
+require_once "./app/functionUtil.php";
+
 /*******
  *        $_GET Pega todas as informacoes do tipo GET, mas o Metodo GET não é recomendado para enviar dados sensiveis porque
  *   ele mostrado o que esta sendo passado pela URL
@@ -13,26 +16,55 @@
  *
  *        var_dump($_POST); //var_dump serve para ver o que esta sendo passado pelo formulario
  */
-    $email = $_POST['email-usuario-cadastro']; //criar uma variavel, usa o metodo POST pegar as informacoes que virão do input email-usuario-cadastro
-    $nome = $_POST['nome-usuario-cadastro'];
-    $usuario = $_POST['usuario-cadastro'];
-    $senha = $_POST['senha-cadastro'];
+    $email = getPost("email-cadastro"); //criar uma variavel, usa o metodo POST para pegar as informacoes que virão do input email-usuario-cadastro da index
+    $nome = getPost("nome-cadastro");
+    $usuario = getPost("usuario-cadastro");
+    $senha = getPost("senha-cadastro");
+    $concordo = getPost("remember");
 
-    $databaseNome = 'root'; //Nome do usuario e senha para fazer a conexão com MySQL
-    $databaseSenha = '';
+    //VERIFICA SE OS CAMPOS ESTÃO PREENCHIDOS, SE NÃO AVISA ERRO, as funcões EMPTY = VAZIO ou !isset = não definido
+    $messages = ""; //variavel que vai receber mais mensagens de erro
+    if(empty($email)){ //funcao !isset verificada se essa variavel esta preenchida
+      $messages .= ("<li>E-mail obrigatório</li>"); //Usa o operador de ponto para concatenar as mensagens de erro, caso sejam sucessivas
+    }
+    if(empty($nome)){ //funcao !isset verificada se essa variavel esta preenchida
+      $messages .= ("<li>Nome obrigatório</li>");
+    }
+    if(empty($usuario)){ //funcao !isset verificada se essa variavel esta preenchida
+      $messages .= ("<li>Usuário obrigatório</li>");
+    }
+    if(empty($senha)){ //funcao !isset verificada se essa variavel esta preenchida
+      $messages .= ("<li>Senha obrigatória</li>");
+    }
+    if(!isset($concordo)){
+      $messages .= ("<li>Concorde com as politicas de uso</li>");
+    }
 
-    try {
-      $conn = new PDO('mysql:host=localhost;dbname=instaclone',
-                      $databaseNome, $databaseSenha); // ---------------> Cria e instancia uma classe de conexao PDO e passa URL, usuario e senha
-      $sql = ' INSERT INTO usuarios(email, nome, usuario, senha) '.
-             ' VALUES (:nome, :email, :usuario, :senha)' ; // ----------> Cria uma Query SQL e associa a coluna do banco com as variaveis dos dados
-      $stmt = $conn->prepare($sql); // ---------------------------------> Faz o PreparedStatement com a conexao passando a Query SQL
+    if(strlen($messages) > 0){ //Se caso a variavel menssages for preenchida, tera erros para retornar ao usuario
+      $messages = "<ul>".$messages."</ul>";
+      toSession("messages", $messages); //Inicia uma sessão com as mensagens de erro
+      toSession("email", $email); //Salva os dados ja preenchidos para o usuario não precisar preencher novamente
+      toSession("nome", $nome);
+      toSession("usuario", $usuario);
+
+      header("Location: ./index.php"); //função header direciona para alguma pagina desde que não tenha usado a funcao echo de escrever
+      exit();
+    }
+
+    try{
+      $sql = "INSERT INTO usuarios(email, nome, usuario, senha) VALUES (:email, :nome, :usuario, :senha)" ; // ----------> Cria uma Query SQL e associa a coluna do banco com as variaveis dos dados
+      $stmt = getConnection()->prepare($sql); // ---------------------------------> Faz o PreparedStatement com a conexao passando a Query SQL
       $stmt->bindParam(':email', $email); // ---------------------------> Preenche os campos das colunas para enviar para o banco
       $stmt->bindParam(':nome', $nome);
       $stmt->bindParam('usuario', $usuario);
       $stmt->bindParam(':senha', $senha);
-      $stmt->execute(); //----------------------------------------------> Executa a SQL, se não ter certo retornará um erro que o catch ira pegar
-      echo 'Tudo certo';
+      //$stmt->execute(); //----------------------------------------------> Executa a SQL, se não ter certo retornará um erro que o catch ira pegar
+      if($stmt->execute()){ //-------------------> Verifica se SQL se retorna true deu certo, se não falhou
+        echo "Tudo certo";
+      }
+      else{
+        echo "falhou";
+      }
     }catch(PDOException $e) {
       echo 'Erro: ' . $e->getMessage();
     }
--- InstaClone/index.php
+++ InstaClone/index.php
@@ -1,8 +1,10 @@
+<?php require_once "./app/functionUtil.php";?>
 <!DOCTYPE html>
 <html lang="pt" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="./estilo.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Roboto|Sofia" rel="stylesheet">
     <title>Projeto</title>
   </head>
@@ -14,12 +16,13 @@
       <span id="clone">Clone</span>
     </div>
     <div class="object-panel-title align-right">
-      <forms>
+      <forms method="POST" action="login.php">
         <div class="object-panel-title">
-          Login: <input type="text" class="text-login">
+          Login: <input type="text" class="text-login" name="login">
         </div>
         <div class="object-panel-title">
-          Senha: <input type="password" class="password-login">
+          Senha: <input type="password" class="password-login" name="senha">
+          <input type="checkbox" checked="checked" name="remember">Lembre-me
         </div>
         <div class="object-panel-title">
           <input type="submit" class="buttom-login" value="Entrar">
@@ -28,36 +31,57 @@
     </div>
   </div>
   <div class="panel">
+    <div>
+    <h3 class="error"><?=fromSession("messages");?></h3>
+    </div>
     <header class="titulo">
       <p><b>Cadastre-­se para ver fotos e vídeos dos seus amigos.</b></p>
     </header>
     <div class="formulario">
-      <form>
+      <form method="POST" action="cadastro.php">
         <div>
-          <input type="text" class="text text-focus" name="email-usuario-cadastro" placeholder="Numero do celular ou e-mail">
+          <input type="email" value="<?=fromSession("email"); ?>" class="text text-focus" name="email-cadastro" placeholder="Numero do celular ou e-mail" id="js_email">
         </div>
         <div>
-          <input type="text" class="text text-focus" name="nome-usuario-cadastro" placeholder="Nome completo">
+          <input type="text" value="<?=fromSession("nome"); ?>" class="text text-focus" name="nome-cadastro" placeholder="Nome completo" id="js_nome">
         </div>
         <div>
-          <input type="text" class="text text-focus" name="usuario-cadastro" placeholder="Nome de Usuario">
+          <input type="text" value="<?=fromSession("usuario"); ?>" class="text text-focus" name="usuario-cadastro" placeholder="Nome de Usuario" id="js_usuario">
         </div>
         <div>
-          <input type="password" class="password password-focus" name="senha-cadastro" placeholder="Senha">
+          <input type="password" class="password password-focus" name="senha-cadastro" placeholder="Senha" id="js_senha">
         </div>
         <div>
           <label class="container">Concordo com os termos e a politicas de dados e cookies
-            <input type="checkbox" checked="checked">
+            <input type="checkbox" name="remember">
             <span class="checkmark"></span>
           </label>
         </div>
         <div>
-          <input  class="buttom buttom-focus" value="Cadastre-se">
+          <input  type="submit" class="buttom buttom-focus" value="Cadastre-se"> <!-- onclick="exibir()"-->
         </div>
       </form>
     </div>
   </div>
   <!--
+  <script language="javascript">
+    var email;
+    var nome;
+    var usuario;
+    var senha;
+    var informacao;
+
+    function exibir(){
+       email = Document.getElementById("js_email");
+       nome = Document.getElementById("js_nome");
+       usuario = Document.getElementById("js_usuario");
+       senha = Document.getElementById("js_senha");
+
+       informacao = email + ' ' + nome + ' ' + usuario + ' ' + senha;
+
+       Document.write(informacao);
+    }
+  </script>
   <div class="panel">
     <h3>Já tem conta?</h3>
     <a href="login.php">Conecte-se</a>
